fcb758f07abc2d2f9bbfc4781d1e2d82f8613821
eeddee <edward@edwardsharp.net>
2019-11-03 23:24:16 -0500

    property map, filters, post query

--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -1,35 +1,39 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import MaterialTable from 'material-table'
 import ImportDialog from './ImportDialog'
-import { AppDatabase, IProduct } from '../appDatabase'
 
-// import { useAllDocumentsService } from '../services/useDocServices'
-// import { useProductDocService } from '../services/useDocServices'
-// import { productMapFn } from '../util/utilz'
-// import { ProductDoc } from '../types/Product'
+const PROPERTY_MAP: { [index: string]: string } = {
+  a: 'Artificial ingredients',
+  c: 'Low carb',
+  d: 'Dairy free',
+  f: 'Food Service items',
+  g: 'Gluten free',
+  k: 'Kosher',
+  l: 'Low sodium/no salt',
+  m: 'Non-GMO Project Verified ',
+  og: 'Organic',
+  r: 'Refined sugar',
+  v: 'Vegan',
+  w: 'Yeast free',
+  ft: 'Fair Trade',
+  n: 'Natural',
+  s: 'Specialty Only',
+  y: 'Yeast free',
+  1: '100% organic',
+  2: '95%+ organic',
+  3: '70%+ organic'
+}
+
+function renderCodes(codes: string) {
+  return codes
+    .split(', ')
+    .map(code => (PROPERTY_MAP[code] ? PROPERTY_MAP[code] : ''))
+    .join(', ')
+}
 
 function DataTable() {
-  // const db = new AppDatabase()
-
   const [importDialogOpen, setImportDialogOpen] = useState(false)
 
-  /* 
-  "id": 1,
-    "unf": "012850-4",
-    "upc_code": "0-75062-50214-4",
-    "category": "Bulk Foods",
-    "sub_category": "BULK SNACKS-TRAILMIXES-CEREALS",
-    "name": null,
-    "description": "Mix; Honeynut & Seed Crunch",
-    "pk": 10,
-    "size": "#",
-    "unit_type": "CS",
-    "ws_price": "33.64",
-    "u_price": "3.36",
-    "codes": "f, n",
-    "createdAt": "2019-11-01T21:11:57.343Z",
-    "updatedAt": "2019-11-01T21:11:57.343Z"
-    */
   return (
     <>
       <MaterialTable
@@ -45,7 +49,13 @@ function DataTable() {
           { title: 'unit_type', field: 'unit_type', type: 'string' },
           { title: 'price', field: 'ws_price', type: 'currency' },
           { title: 'unit_price', field: 'u_price', type: 'currency' },
-          { title: 'codes', field: 'codes', type: 'string' },
+          {
+            title: 'codes',
+            field: 'codes',
+            type: 'string',
+            lookup: PROPERTY_MAP,
+            render: row => renderCodes(row.codes)
+          },
           {
             title: 'category',
             field: 'category',
@@ -55,60 +65,34 @@ function DataTable() {
             title: 'sub category',
             field: 'sub_category',
             type: 'string'
-          },
-          { title: 'upc', field: 'upc_code', type: 'string' },
-          { title: 'unf', field: 'unf', type: 'string' }
+          }
+          // { title: 'upc', field: 'upc_code', type: 'string' },
+          // { title: 'unf', field: 'unf', type: 'string' }
         ]}
-        // data={query => {
-        //   console.log('QUERY:', query)
-
-        //   return new Promise(async (resolve, reject) => {
-        //     const regex: RegExp = new RegExp(query.search, 'i')
-
-        //     let data: any = await db.products.orderBy(
-        //       (query.orderBy && query.orderBy.field) || ''
-        //     )
-        //     if (query.search && query.search !== '') {
-        //       data = await data.filter((r: IProduct) => regex.test(r.search))
-        //     }
-
-        //     if (query.orderDirection === 'desc') {
-        //       console.log('GONNA .reverse()!')
-        //       data = await data.reverse()
-        //     }
-
-        //     data = await data
-        //       .offset(query.page)
-        //       .limit(query.pageSize)
-        //       .toArray()
-
-        //     resolve({
-        //       data: data as IProduct[],
-        //       page: query.page,
-        //       totalCount: await db.products
-        //         .filter(r => regex.test(r.search))
-        //         .count()
-        //     })
-        //   })
-        // }}
         data={query =>
           new Promise((resolve, reject) => {
             console.log('query:', query)
-            let url = 'http://localhost:3000/products'
-            url += '?limit=' + query.pageSize
-            url += '&page=' + query.page
-            // #TODO: use query params lib
-            if (query.orderBy && query.orderBy.field) {
-              url += '&orderBy=' + query.orderBy.field
-            }
-            if (query.orderDirection) {
-              url += '&orderDirection=' + query.orderDirection
-            }
-            if (query.search) {
-              url += '&q=' + query.search
-            }
+            // let url = 'http://localhost:3000/products'
+            // url += '?limit=' + query.pageSize
+            // url += '&page=' + query.page
+            // // #TODO: use query params lib
+            // if (query.orderBy && query.orderBy.field) {
+            //   url += '&orderBy=' + query.orderBy.field
+            // }
+            // if (query.orderDirection) {
+            //   url += '&orderDirection=' + query.orderDirection
+            // }
+            // if (query.search) {
+            //   url += '&q=' + query.search
+            // }
 
-            fetch(url)
+            fetch('http://localhost:3000/products', {
+              method: 'post',
+              headers: {
+                'Content-Type': 'application/json'
+              },
+              body: JSON.stringify(query)
+            })
               .then(response => response.json())
               .then(result => {
                 console.log('result', result)
@@ -122,7 +106,8 @@ function DataTable() {
           maxBodyHeight: 'calc(100vh - 121px)',
           pageSize: 50,
           pageSizeOptions: [50, 100, 500],
-          debounceInterval: 750
+          debounceInterval: 750,
+          filtering: true
         }}
         actions={[
           {
