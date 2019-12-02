3f41e6bd68a0191ebe000f375ab4793c4bcbb71a
eeddee <edward@edwardsharp.net>
2019-12-01 22:01:12 -0500

    Updates

---
+++ precache-manifest.a1a1e18f3e5f991cd570f7e543cc9ce6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07845bd7e92915837a33babc0c76d049",
+    "url": "/index.html"
+  },
+  {
+    "revision": "515688b23873f8b85fcd",
+    "url": "/static/css/main.17897384.chunk.css"
+  },
+  {
+    "revision": "8956cb195b81bbb7c039",
+    "url": "/static/js/2.60d76b88.chunk.js"
+  },
+  {
+    "revision": "515688b23873f8b85fcd",
+    "url": "/static/js/main.da427006.chunk.js"
+  },
+  {
+    "revision": "2257e64a1218c0bf01a7",
+    "url": "/static/js/runtime-main.dd4294af.js"
+  }
+]);
