1445b15794bc7ef4d7e1184d0397b535de80ac9b
edward sharp <edward@edwardsharp.net>
2022-12-24 17:54:13 -0500

    hardcode 'em for cloudflare pages :fried_shrimp:

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,8 +1,11 @@
 import { createClient } from '@supabase/supabase-js'
 import { Database } from '../types/supabase'
 
-const SUPABASE_URL = process.env.MARSH_SUPABASE_URL || ''
-const SUPABASE_ANON = process.env.MARSH_SUPABASE_ANNON || ''
+const SUPABASE_URL =
+  process.env.MARSH_SUPABASE_URL || 'https://zlzhojhgkapokbmvokzz.supabase.co'
+const SUPABASE_ANON =
+  process.env.MARSH_SUPABASE_ANNON ||
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpsemhvamhna2Fwb2tibXZva3p6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzE0MDAzMjcsImV4cCI6MTk4Njk3NjMyN30.hKX7IsGxyKQbg59uKK28C_-Xy_FESgnHJQ0LSqNYe7k'
 export const SUPABASE_SERVICE_ROLE_KEY =
   process.env.MARSH_SUPABASE_SERVICE_ROLE_KEY || ''
 
